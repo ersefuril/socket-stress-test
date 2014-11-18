996f206bc15ceda4419112e5ea1f4bcbb5db097a
chivot.r <chivot.r@sfeir.lu>
2014-11-18 18:26:06 +0100

    [ Err...  server was missing. ] - Add missing files

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 3001;
+
+app.use(express.static(__dirname + '/client'));
+
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
+
+io.on('connection', function(socket) {
+
+    console.log(socket.id);
+
+    socket.on('start', function(nbMessage) {
+        console.log("*** Start Test ***");
+        console.log("* Broadcast " + nbMessage + " messages");
+        var i;
+        for(i = 0; i < nbMessage; i++) {
+            socket.emit('message');
+        }
+
+    });
+
+    /*
+    socket.on('question', function() {
+        socket.emit('answer');
+    });
+
+    socket.on('disconnect', function () {  });
+    */
+});
